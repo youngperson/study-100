9ba584d2da5e1f387c46a6106f9fb7a5484b6057
wanrenliang <wanrenliang@luojilab.com>
2018-10-12 18:59:57 +0800

    GZIP

---
+++ experiment/tools/gzip/gzip.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io/ioutil"
+)
+
+// gzip压缩
+func GzipEncode(in []byte) ([]byte, error) {
+	var (
+		buffer bytes.Buffer
+		out    []byte
+		err    error
+	)
+	writer := gzip.NewWriter(&buffer)
+	_, err = writer.Write(in)
+	if err != nil {
+		writer.Close()
+		return out, err
+	}
+	err = writer.Close()
+	if err != nil {
+		return out, err
+	}
+
+	return buffer.Bytes(), nil
+}
+
+// gzip解码
+func GzipDecode(in []byte) ([]byte, error) {
+	reader, err := gzip.NewReader(bytes.NewReader(in))
+	if err != nil {
+		var out []byte
+		return out, err
+	}
+	defer reader.Close()
+
+	return ioutil.ReadAll(reader)
+}
